5a9eabaca861a0d66c68853e888a70d96c835f78
Andrew <andrew@bimshift.com>
2013-09-18 18:18:42 -0600

    Include CodeIgniter

---
+++ application/views/pages/home.php
@@ -0,0 +1,1 @@
+<h2>Welcome to acmxlabs.org.</h2>
---
+++ application/views/templates/header.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+<html> 
+<head>
+<title>acmxlabs</title>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700|Open+Sans:400italic,400,300,700' rel='stylesheet' type='text/css' />
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+<link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="js/bootstrap.js"></script>
+</head>
+<body>
+<div class="container">
+<div id="header">
+<a href="#">acmx<span id="red">labs</span><span id="grey">.org</span></a>
+<div id="buttons">
+</div>
+</div>
