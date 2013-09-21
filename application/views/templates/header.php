b6dd7c320a4d8a4087a9e4992b0a4f46790a88f7
Andrew <andrew@bimshift.com>
2013-09-21 13:36:00 -0600

    Fix routing issues, create .htaccess, create 'about', 'projects', and 'support' pages.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,10 +3,10 @@
 <head>
 <title>acmxlabs</title>
 <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700|Open+Sans:400italic,400,300,700' rel='stylesheet' type='text/css' />
-<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
-<link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+<link rel="stylesheet" href="/css/bootstrap.css" type="text/css" />
+<link rel="stylesheet" href="/css/style.css" media="screen" type="text/css" />
 <script src="http://code.jquery.com/jquery-latest.js"></script>
-<script src="js/bootstrap.js"></script>
+<script src="/js/bootstrap.js"></script>
 </head>
 <body>
 <div class="container-fluid">
@@ -14,11 +14,11 @@
 	<div class="span10 offset1">
 		<div class="page-header" id="header">
 			<h1>
-				<a id="logo" href="#">acmx<span id="red">labs</span><span id="grey">.org</span></a>
+				<a id="logo" href="/home">acmx<span id="red">labs</span><span id="grey">.org</span></a>
 				<div class="btn-group pull-right">
-						<a class="btn btn-large btn-info" href="#">who we are</a> 
-						<a class="btn btn-large btn-info" href="#">our projects</a> 
-						<a class="btn btn-large btn-success" href="#">support us</a>
+						<a class="btn btn-large btn-info" href="/about">who we are</a> 
+						<a class="btn btn-large btn-info" href="/projects">our projects</a> 
+						<a class="btn btn-large btn-success" href="/support">support us</a>
 				</div>
 			</h1>
 		</div>
