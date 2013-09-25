0003bfb3f181788eebc15205e096ebe0a1838934
Andrew <andrew@bimshift.com>
2013-09-24 19:41:48 -0600

    Updated CSS

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,30 +1,30 @@
-	<div class="row-fluid">
-		<div class="span10 offset1">
-			<div class="row-fluid">
-				<div class="span4 square">
-					BOX1
-				</div>
-				<div class="span4 square">
-					BOX2
-				</div>
-				<div class="span4 square">
-					BOX3
-				</div>
+<div class="row-fluid">
+	<div class="span10 offset1">
+		<div class="row-fluid">
+			<div class="span4 square">
+				BOX1
+			</div>
+			<div class="span4 square">
+				BOX2
+			</div>
+			<div class="span4 square">
+				BOX3
 			</div>
 		</div>
 	</div>
-	<div class="row-fluid">
-		<div class="span10 offset1">
-			<div class="row-fluid">
-				<div class="span4 square">
-					BOX4
-				</div>
-				<div class="span4 square">
-					BOX5
-				</div>
-				<div class="span4 square">
-					BOX6
-				</div>
+</div>
+<div class="row-fluid">
+	<div class="span10 offset1">
+		<div class="row-fluid">
+			<div class="span4 square">
+				BOX4
+			</div>
+			<div class="span4 square">
+				BOX5
+			</div>
+			<div class="span4 square">
+				BOX6
 			</div>
 		</div>
 	</div>
+</div>
