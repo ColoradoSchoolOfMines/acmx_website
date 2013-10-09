de4635cf34dd349dc0bfe2eb5c4eb7149f45b14e
Andrew <andrew@bimshift.com>
2013-10-08 19:33:18 -0600

    Create project display logic

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -5,7 +5,22 @@ class Pages extends CI_Controller {
 	{
 		if(!file_exists('application/views/pages/'.$page.'.php'))
 		{
-			show_404();
+			$this->load->database('acmx');
+			$query = $this->db->get('projects');
+			if($row = $query->result_array()){
+				$names = [];
+				foreach($row['name'] as $key->$name){
+					$names[$key] = preg_replace('/[^a-z]/', '', strtolower($name));
+				}
+				if(in_array($page, $names){
+					$data['project'] = $page;
+					$this->load->view('templates/header',$data);
+					$this->load->view('pages/project',$data);
+					$this->load->view('templates/footer',$data);
+				}else{
+					show_404();
+				}
+			}
 		}
 
 		$data['title'] = ucfirst($page);
