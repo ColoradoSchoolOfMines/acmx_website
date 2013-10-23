89491bb393345957344171cb3242d74b6ffb51a0
Andrew <andrew@bimshift.com>
2013-10-22 18:42:22 -0600

    Add logo

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -14,7 +14,7 @@
 	<div class="span12">
 		<div class="page-header" id="header">
 			<h1>
-				<a id="logo" href="/">acmx<span id="red">labs</span><span id="grey">.org</span></a>
+				<a id="logo" href="/"><img src="/acmx.jpeg" /></a>
 				<div class="btn-group pull-right" id="widemenu">
 						<a class="btn btn-large btn-info" href="/about">who we are</a> 
 						<a class="btn btn-large btn-info" href="/projects">our projects</a> 
@@ -22,11 +22,11 @@
 				</div>
 				<div class="btn-group pull-right" id="thinmenu">
 					<a class="btn btn-large dropdown-toggle btn-info" data-toggle="dropdown" href="#"><span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="/about">who we are</a></li>
-							<li><a href="/projects">our projects</a></li>
-							<li><a href="/support">support us</a></li>
-						</ul>
+					<ul class="dropdown-menu">
+						<li><a href="/about">who we are</a></li>
+						<li><a href="/projects">our projects</a></li>
+						<li><a href="/support">support us</a></li>
+					</ul>
 				</div>
 			</h1>
 		</div>
