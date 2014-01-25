188d249f55f018cac23d07e6e4b3ec8938bb094f
george <rpubaddr0@gmail.com>
2014-01-25 17:09:13 +0000

    Working on adding user models and linking them with projects.

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,9 @@ class Project(models.Model):
     project_id = models.CharField(max_length=50)
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    team = models.ManyToManyField(UserProfile)
+    pub_date = models.DateTimeField('date published', auto_now=True,
+            auto_now_add=True)
     
     def __unicode__(self):
         return self.title
@@ -17,3 +19,21 @@ class Project(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    projects = models.ManyToManyField(Project)
+
+    SEMESTER_CHOICES = (
+            (SPRING, 'Spring'),
+            (FALL, 'Fall'),
+    )
+
+    email = models.CharField(max_length=200)
+    phone = models.CharField(_('Number'), maxlength = 40)
+    homepage = models.URLFiels(max_length=200)
+    major = models.CharField(max_length=200)
+    grad_year = models.PositiveSmallIntegerField()
+    grad_semester = models.CharField(max_length=10,
+            choices=SEMESTER_CHOICES)
+
