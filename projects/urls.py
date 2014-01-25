c49c96e85c8e08ecde6f3bc2bd4c2d5bca3c19c5
george <rpubaddr0@gmail.com>
2014-01-25 06:03:32 +0000

    Updated urls.py, removed database.sql.

--- projects/urls.py
+++ projects/urls.py
@@ -3,13 +3,8 @@ from projects import views
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^about/$', views.AboutView.as_view(), name='about'),
-    url(r'^contact/$', views.ContactView.as_view(), name='contact'),
-    url(r'^support/$', views.SupportView.as_view(), name='support'),
-    url(r'^project/(?P<pk>\w+)/$', views.DetailView.as_view(), name='detail'),
-#    url(r'^admin/$', views.AdminView.as_view(), name="admin"),
-#    url(r'^project/(?P<project_id>\d+)/$', views.DetailView.as_view(), name='detail'),
-
-	# 404 reroute has to be last
-	# url(r'^.*$', views.IndexView.as_view(), name='index'),	# return bad requests to the index page
+    url(r'^about$', views.AboutView.as_view(), name='about'),
+    url(r'^contact$', views.ContactView.as_view(), name='contact'),
+    url(r'^support$', views.SupportView.as_view(), name='support'),
+    url(r'^p/(?P<pk>\w+)/$', views.DetailView.as_view(), name='detail'),
 )
