b13cea14cea1869aa5ecdfff44fd3f8df2e48a7d
george <rpubaddr0@gmail.com>
2014-08-15 15:10:13 -0600

    Add project urls to the project.

--- acmx_website/urls.py
+++ acmx_website/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'acmx_website.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include('projects.urls', namespace='projects')),
     url(r'^admin/', include(admin.site.urls)),
 )
