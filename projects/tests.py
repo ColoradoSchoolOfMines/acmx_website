102966a4c4d05c18396a4deb063298527e4b7f1d
george <rpubaddr0@gmail.com>
2014-08-27 15:17:08 -0600

    Added unit test and Travis config.

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,9 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_projects_index(self):
+        found = resolve('/')
+        self.assertEqual(found.view_name, 'projects:index')
